d1fe30b22873b32919a1fa2bb647a3e3ca4e3a99
Fabien Hance <piperley@hotmail.com>
2021-09-06 22:42:45 +0200

    modal motion

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -13,7 +13,11 @@ const ImageGrid = ({setSelectedImg}) => {
           layout
           whileHover={{ opacity: 1 }}
           onClick={()=> setSelectedImg(doc.url)}>
-            <img src={doc.url} alt="uploaded  pic"/>
+            <motion.img src={doc.url} alt="uploaded  pic"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
+            />
           </motion.div>
       ))}
   </div>;
