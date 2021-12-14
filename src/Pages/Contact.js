79e73a8c286da589d1b6365aa80f97440e36a72d
Fabien Hance <piperley@hotmail.com>
2021-12-14 15:45:41 +0100

    css title

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,26 +1,20 @@
 import React from "react";
 import Title from "../components/Title";
-import ButtonMailto from"../components/ButtonMailto";
-
-
-
+import ButtonMailto from "../components/ButtonMailto";
 
 const Contact = ({ mailto, label }) => {
-
-
   return (
     <>
-     <div className="App">
-    <Title name="Let's talk" />
-
-      let's start the conversation and make something happen:
-      <span> </span>
-      
-      <ButtonMailto  label="contact@fabienhance.com" mailto="mailto:contact@fabienhance.com" />
-     
-    </div>
-   
-  </>
+      <div className="App">
+        <Title name="Let's talk" />
+        let's start the conversation and make something happen:
+        <span> </span>
+        <ButtonMailto
+          label="contact@fabienhance.com"
+          mailto="mailto:contact@fabienhance.com"
+        />
+      </div>
+    </>
   );
 };
 
